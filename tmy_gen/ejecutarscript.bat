34943699a1f31307c74edcd673b38d7d7ec05597
gavg712 <gavg712@gmail.com>
2015-07-13 09:48:48 -0500

    Ejemplo bat para ejecutar el codigo

--- tmy_gen/ejecutarscript.bat
+++ tmy_gen/ejecutarscript.bat
@@ -1,5 +1,8 @@
 @echo off
 C:
+rem Path to executables of R
 PATH c:\Program Files\R\R-3.2.0\bin\;%path%
+rem Path to tmy generator script
 cd C:\Users\Analista INER\Desktop\tmy_gen
+rem 'code.r' name of script
 Rscript code.r 1
